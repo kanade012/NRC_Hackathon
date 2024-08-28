1e71f22c569698d3d7a1af3d6d136d23ee7431c9
YoungHa Chang <wkddudgk4869@gmail.com>
2024-08-28 16:55:41 +0900

    [feat] 실시간 음성 감지 및 인공지능 모델 연동

--- lib/Page/Alarm_Screen.dart
+++ lib/Page/Alarm_Screen.dart
@@ -1,10 +1,13 @@
 import 'dart:async';
-
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:screen_brightness/screen_brightness.dart';
-import 'package:vibration/vibration.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
 import '../Config/Setting_Provider.dart';
 import '../Config/color.dart';
 import '../main.dart';
@@ -19,16 +22,121 @@ class Alarm extends StatefulWidget {
 
 class _AlarmState extends State<Alarm> {
   late String _timeString;
+  String _detectionResult = "Noise";
+  final FlutterSoundRecorder _recorder = FlutterSoundRecorder();
+  late Interpreter _interpreter;
+  late TensorBuffer _inputBuffer;
+  late TensorBuffer _outputBuffer;
+  List<int> _pcmDataBuffer = [];
+  bool _isRecording = false;
+  Timer? _timer;
+  StreamController<Food> _streamController = StreamController<Food>();
 
   @override
   void initState() {
     super.initState();
     _loadPreferences();
+    _initializeInterpreter();
     _timeString = _formatTime(DateTime.now());
     Timer.periodic(Duration(seconds: 1), (Timer t) => _getTime());
   }
 
-  // SharedPreferences에서 저장된 값을 불러오는 함수
+  void _initializeInterpreter() async {
+    try {
+      _interpreter = await Interpreter.fromAsset('soundAnalyzer.tflite');
+      _inputBuffer = TensorBuffer.createFixedSize([1, 1640], TfLiteType.float32);
+      _outputBuffer = TensorBuffer.createFixedSize([1, 3], TfLiteType.float32);
+      _startContinuousRecording();
+    } catch (e) {
+      print("Error initializing interpreter: $e");
+    }
+  }
+
+  void _startContinuousRecording() async {
+    await Permission.microphone.request();
+
+    await _recorder.openRecorder();
+    _isRecording = true;
+
+    await _recorder.startRecorder(
+      toStream: _streamController.sink,
+      codec: Codec.pcm16,
+    );
+
+    _streamController.stream.listen((Food foodData) {
+      if (foodData is FoodData && _isRecording) {
+        _onDataReceived(foodData);
+      }
+    });
+
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (_pcmDataBuffer.isNotEmpty) {
+        _processAudioData(Uint8List.fromList(_pcmDataBuffer));
+        _pcmDataBuffer.clear();
+      }
+    });
+  }
+
+  void _onDataReceived(FoodData foodData) {
+    _pcmDataBuffer.addAll(foodData.data!);
+  }
+
+  void _processAudioData(Uint8List pcmData) {
+    Float32List floatBuffer = _convertToFloat32(pcmData);
+
+    if (floatBuffer.length < 1640) {
+      print("Padding floatBuffer from ${floatBuffer.length} to 1640 samples");
+      floatBuffer = Float32List.fromList(
+          floatBuffer.toList() + List.filled(1640 - floatBuffer.length, 0.0));
+    } else if (floatBuffer.length > 1640) {
+      print("Trimming floatBuffer from ${floatBuffer.length} to 1640 samples");
+      floatBuffer = floatBuffer.sublist(0, 1640);
+    }
+
+    _inputBuffer.loadList(floatBuffer, shape: [1, 1640]);
+    _interpreter.run(_inputBuffer.buffer, _outputBuffer.buffer);
+
+    var outputData = _outputBuffer.getDoubleList();
+    _handleModelOutput(outputData);
+  }
+
+  Float32List _convertToFloat32(Uint8List pcmData) {
+    var buffer = Float32List(pcmData.length ~/ 2);
+    for (int i = 0; i < pcmData.length; i += 2) {
+      buffer[i ~/ 2] = (pcmData[i] | pcmData[i + 1] << 8).toSigned(16).toDouble() / 32768.0;
+    }
+
+    print("PCM to Float32 conversion complete: ${buffer.length} samples");
+    return buffer;
+  }
+
+  void _handleModelOutput(List<double> outputData) {
+    print("Model output: $outputData");
+
+    int maxIndex = outputData.indexWhere((element) => element == outputData.reduce((a, b) => a > b ? a : b));
+
+    setState(() {
+      if (maxIndex == 0) {
+        _detectionResult = "Hoot";
+      } else if (maxIndex == 1) {
+        _detectionResult = "Noise";
+      } else if (maxIndex == 2) {
+        _detectionResult = "Siren";
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _isRecording = false;
+    _timer?.cancel();
+    _recorder.stopRecorder();
+    _recorder.closeRecorder();
+    _interpreter.close();
+    _streamController.close();
+    super.dispose();
+  }
+
   Future<void> _loadPreferences() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     final settingsProvider =
@@ -40,25 +148,12 @@ class _AlarmState extends State<Alarm> {
       settingsProvider.textSize = prefs.getDouble('textSize') ?? 26.0;
       settingsProvider.transitionTime = prefs.getInt('transitionTime') ?? 1;
     });
-    ///진동 기능
-    // if (await Vibration.hasVibrator() ?? false) {
-    //   int power = (settingsProvider.vibrationIntensity * 1000).round();
-    //   Vibration.vibrate(duration: power);
-    // } else {
-    //   ScaffoldMessenger.of(context).showSnackBar(
-    //     SnackBar(content: Text("진동 기능이 없습니다")),
-    //   );
-    // }
-    // 화면 밝기를 설정합니다.
     _updateBrightness(settingsProvider.brightness);
   }
 
-  // 화면 밝기를 업데이트하는 함수
   Future<void> _updateBrightness(double brightness) async {
     try {
-      // brightness 값을 0.0 ~ 1.0 범위로 조정
       double normalizedBrightness = brightness / 10.0;
-
       await ScreenBrightness().setScreenBrightness(normalizedBrightness);
     } catch (e) {
       print("Failed to set brightness: $e");
@@ -69,9 +164,9 @@ class _AlarmState extends State<Alarm> {
     final DateTime now = DateTime.now();
     final String formattedTime = _formatTime(now);
     if (mounted)
-    setState(() {
-      _timeString = formattedTime;
-    });
+      setState(() {
+        _timeString = formattedTime;
+      });
   }
 
   String _formatTime(DateTime dateTime) {
@@ -90,7 +185,6 @@ class _AlarmState extends State<Alarm> {
           body: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              // 현재 시간
               Column(
                 children: [
                   Text(
@@ -107,17 +201,15 @@ class _AlarmState extends State<Alarm> {
                   ),
                 ],
               ),
-              // 사운드 판별 결과
               Text(
-                "Knock",
+                _detectionResult,
                 style: TextStyle(
                     fontSize: settingsProvider.textSize,
                     color: DelightColors.grey1,
                     fontWeight: FontWeight.w900),
               ),
-              // 음성 진폭도 판별
               Container(
-                height: ratio.height * 100,
+                  height: ratio.height * 100,
                   child: SoundWaveformWidget())
             ],
           ),
@@ -132,10 +224,10 @@ class _AlarmState extends State<Alarm> {
             body: Container(
               child: Center(
                   child: Icon(
-                Icons.arrow_back,
-                size: ratio.width * 200,
-                color: DelightColors.grey1,
-              )),
+                    Icons.arrow_back,
+                    size: ratio.width * 200,
+                    color: DelightColors.grey1,
+                  )),
             ),
           ),
         ),
